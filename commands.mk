47c91fefd0593226d43341ea18f67f068023ce42
Matthieu XILLO <matthieu.xillo@hotmail.com>
2022-05-06 19:04:00 +0200

    Ajout de fichiers .mk contenant les commandes communes aux Makefile pour éviter les redondances

---
+++ commands.mk
@@ -0,0 +1,22 @@
+OUTPUT=$(NAME).pdf
+SRC=$(SRC_DIR)/$(NAME).tex
+
+$(NAME).pdf: $(SRC_DIR)/$(NAME).tex $(INCLUDE)
+	(cd src && pdflatex $(CFLAGS) $(NAME).tex)
+	mv $(OBJ_DIR)/$(OUTPUT) ./
+
+view: $(NAME).pdf
+	evince $(VFLAGS) $(OUTPUT)
+
+adapt:
+	-mv $(SRC_DIR)/* $(SRC_DIR)/$(NAME).tex
+	-mv *.pdf $(NAME).pdf
+	-rm -r $(OBJ_DIR)/*
+	sed -i "1s:.*:\\\\newcommand\\\\PATH{$$(pwd)}:" $(SRC)
+
+hide:
+	sed -i "1s:.*:\\\\newcommand\\\\PATH{Lancez \`make adapt avant de compiler!\`}:" $(SRC)
+	
+clean:
+	-rm -r *.pdf
+	rm -r $(OBJ_DIR)/*
---
+++ defs.mk
@@ -0,0 +1,8 @@
+SRC_DIR=src
+OBJ_DIR=obj
+INC_DIR=include
+
+INCLUDE=$(wildcard $(INC_DIR)/*.tex)
+
+CFLAGS=--shell-escape --output-directory ../$(OBJ_DIR)
+VFLAGS=--fullscreen
